c539f87f9c219f92513dff2bb38bad228821d3f9
Yang Yang <reliveyy@gmail.com>
2020-12-16 21:21:20 +0800

    feat: update image tags
    - mainnet boltz:latest -> boltz:1.2.0
    - mainnet proxy:latest__proxy-first-fix -> proxy:1.2.0
    - testnet/simnet proxy:latest__proxy-first-fix -> proxy:latest

--- service/config.go
+++ service/config.go
@@ -8,7 +8,7 @@ var (
 			"connext": "connextproject/vector_node:837bafa1",
 			"arby":    "exchangeunion/arby:latest",
 			"webui":   "exchangeunion/webui:latest",
-			"proxy":   "exchangeunion/proxy:latest__proxy-first-fix",
+			"proxy":   "exchangeunion/proxy:latest",
 			"xud":     "exchangeunion/xud:latest",
 		},
 		"testnet": {
@@ -21,7 +21,7 @@ var (
 			"arby":      "exchangeunion/arby:latest",
 			"boltz":     "exchangeunion/boltz:latest",
 			"webui":     "exchangeunion/webui:latest",
-			"proxy":     "exchangeunion/proxy:latest__proxy-first-fix",
+			"proxy":     "exchangeunion/proxy:latest",
 			"xud":       "exchangeunion/xud:latest",
 		},
 		"mainnet": {
@@ -32,9 +32,9 @@ var (
 			"lndltc":    "exchangeunion/lndltc:0.11.0-beta.rc1",
 			"connext":   "exchangeunion/connext:1.3.6",
 			"arby":      "exchangeunion/arby:1.3.0",
-			"boltz":     "exchangeunion/boltz:latest",
+			"boltz":     "exchangeunion/boltz:1.2.0",
 			"webui":     "exchangeunion/webui:1.0.0",
-			"proxy":     "exchangeunion/proxy:latest__proxy-first-fix",
+			"proxy":     "exchangeunion/proxy:1.2.0",
 			"xud":       "exchangeunion/xud:1.2.0",
 		},
 	}
